cb3d1b7658191e5e1c696b2c0020b2ed0746bbaa
lexyblazy <adekola.olalekan01@gmail.com>
2022-12-19 13:37:47 +0100

    add base exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-console.log("Hello there")
+import { TransactionApi } from "./transaction";
+import { Config } from "./types";
 
+export class Client {
+  transaction: TransactionApi;
+
+  constructor(config: Config) {
+    this.transaction = new TransactionApi(config);
+  }
+}
