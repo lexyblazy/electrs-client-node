ba85fe6daa9217770512582613005708f4f39f72
lexyblazy <adekola.olalekan01@gmail.com>
2022-12-23 12:11:42 +0100

    add new entry point

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
-import { AddressApi } from "./address";
-import { TransactionApi } from "./transaction";
-import { Config } from "./types";
-
-export class Client {
-  transaction: TransactionApi;
-  address: AddressApi;
-
-  constructor(config: Config) {
-    this.transaction = new TransactionApi(config);
-    this.address = new AddressApi(config);
-  }
-}
+export * from "./client";
